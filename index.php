c42467161401d26068bc665029c6062de570745f
Chris Hardcastle <me@chrishardcastle.co.uk>
2016-08-06 15:49:37 +0100

    Removed unused file and unused code in PHP file, which could otherwise be a raw HTML file.

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
-<?php
-	ini_set("display_errors", "on");
-	error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
-	require_once getcwd()."/ContactDirectory.php";
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
